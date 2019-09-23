b82b84eb49244477f5a2dd30b8a49efac893f95d
TJSmiths <55660195+TJSmiths@users.noreply.github.com>
2019-09-23 23:13:42 +0100

    Update ClanPull.py

--- ClanPull.py
+++ ClanPull.py
@@ -2,9 +2,13 @@ import requests
 import json
 import pandas as pd
 
+# Load API Key
+creds = pd.read_csv('API_Key.csv').set_index('key').transpose()
 
-HEADERS = {"X-API-Key":'a024733d647c451b9d7fbaa13d526e38'}
+# Header for GET Request
+HEADERS = {"X-API-Key" : creds.bungie_api[0]}
 
+# Input Clan ID
 print("Insert clanID")
 bungie_api = "https://www.bungie.net/Platform"
 call = "/GroupV2/" + input() + "/Members/"
@@ -15,4 +19,6 @@ results = df.loc['results','Response']
 clan = pd.DataFrame(results,columns = ['memberType','isOnline','lastOnlineStatusChange','groupId','destinyUserInfo',
                                            'bungieNetUserInfo','joinDate'])
 clan['destinyDisplayName'] = clan.apply(lambda x: x.destinyUserInfo['displayName'],axis=1)
+
+#Print the names!
 print(clan['destinyDisplayName'].values)
